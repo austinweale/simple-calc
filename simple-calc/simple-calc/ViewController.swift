c5a8f843aeee053be7814b7f9663029a801579ad
Austin Weale <austinweale@gmail.com>
2016-10-25 12:39:31 -0700

    normal operation buttons added

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
     @IBOutlet var wholeApp: UIView!
     
     @IBOutlet weak var textArea: UILabel!
+    
+    var hasOperator = false
 
     @IBAction func buttonTouch(_ sender: AnyObject) {
         let current = sender.titleLabel!!.text!
@@ -31,13 +33,28 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operation(_ sender: AnyObject) {
-        let current = sender.titleLabel!!.text!
-        let lastInput = self.textArea.text!
-        self.textArea.text = "\(lastInput) \(current) "
+        if(!hasOperator){
+            hasOperator = !hasOperator
+        
+            let current = sender.titleLabel!!.text!
+            let lastInput = self.textArea.text!
+            self.textArea.text = "\(lastInput) \(current) "
+        }
     }
     
     @IBAction func clear(_ sender: AnyObject) {
+        clear()
+    }
+    
+    func clear(){
+        hasOperator = false
         self.textArea.text = ""
     }
+    
+    @IBAction func enterCommand(_ sender: AnyObject) {
+        let expression = self.textArea.text!
+        clear()
+    }
+    
 }
 
